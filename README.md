cb3f9a871393bc1dc6fe08ca8fe9922d28d35b7e
Rinat Sadykov <rsadykov@cozitrip.com>
2018-02-03 17:28:09 +1100

    Version 0.1

---
+++ README.md
@@ -0,0 +1,81 @@
+1)Install all dependencies:
+pip install -r requirements.txt
+
+2) Download a pre-trained facenet model.
+
+This part handles the face -> features conversion. I did not include it in the git repository as it is very large.
+
+The quickest way is to download this folder https://drive.google.com/file/d/0B5MzpY9kBtDVZ2RpVDYwWmxoSUk/edit
+and put the folder under the facenet_model folder.
+
+
+You facenet_model folder should like this for the code to work smoothly
+- 20170512-110547
+    - 20170512-110547.pb
+    - model-20170512-110547.ckpt-250000.data-00000-of-00001
+    - model-20170512-110547.ckpt-250000.index
+    - model-20170512-110547.meta
+
+Other models can be found here https://github.com/davidsandberg/facenet.
+If you wish to use a different model, make sure to modify the code as all references are to hte model in the github link.
+
+
+3) Setup the training data (can skip if using provided data)
+
+For every person's face you want to recognize, please make a folder and put images of photos that include their face (and no one else) in there.
+A folder with training photos of talk host Graham Norton and rapper 50 Cent would look like
+Graham
+	- photo 1
+	- photo 2
+50Cent
+    - photo 1
+    - photo 2
+
+
+4) Align all the faces and crop the images.
+
+Run the following script, if you want to customise the image size it crops to etc you can do so inside the align_dataset_mtcnn.py file.
+See the parse_arguments method for all the different possible commands
+
+src\align\align_dataset_mtcnn.py
+
+Your training_data_aligned folder should now have folders with images inside them. These images have been aligned to make it easier for the facenet network to analyse.
+Without this step facenet will not work correctly.
+
+Note 1: If at the very end your classifier is still having issues, try deleting all the files in the /training_data_aligned folder and realign the images
+Note 2: This code uses MTCNN to identify faces in an image.
+
+5) Train the classifier (SVM Classifier)
+
+Run the following (which is just an easy to use wrapper for classifier.py):
+src\classifier_train.py
+
+For optimal performance you will need to tune the batch size depending on how much memory you have on your system.
+For a small amount of images any configuration works fine.
+
+To see the different commands for fine tuning see the parse_arguments method in src\classifier.py
+
+6) Run on a video (saved video or webcam)
+
+run src\newglint.py
+
+First it downloads a video from YouTube into test_data/video, then runs the facenet + the classifier on the video playing at 5x speed.
+The video will run slowly because running facenet while playing the video is very computationally intensive, especially on a CPU.
+
+A video recording output.avi will be saved which will playback at a faster speed.
+
+If you want to use the webcam as an input or change the video speed or the video, please see the code under
+if __name__ == "__main__":
+
+
+
+# #
+Note that these files/folders are in the git ignore
+test_data/video/*
+training_data_aligned/*
+output.avi
+facenet_model/*
+
+# #
+Note that the images used for training were found public online through Google Image Search and the test video is downloaded from YouTube upon the program running.
+I do not claim any ownership to any of these items and am happy to remove them upon request. Any part of the code which may be against the usage terms of any sites or images is not encouraged to be run and is just there for learning reference.
---
+++ src/newglint.py
@@ -0,0 +1,154 @@
+import tensorflow as tf
+import align.detect_face
+import facenet
+import cv2
+import numpy as np
+import glob
+import pickle
+import collections
+import os
+from pytube import YouTube
+# import ffmpeg
+
+def main(args):
+    minsize = 20
+    threshold = [0.6, 0.7, 0.7]
+    factor = 0.709
+    image_size = 182
+    input_image_size = 160
+
+    # comment out these lines if you do not want video recording
+    # USE FOR RECORDING VIDEO
+    fourcc = cv2.VideoWriter_fourcc(*'X264')
+
+    # Get the path of the classifier and load it
+    classifier_path = os.getcwd() +"\\trained_classifier\\newglint_classifier.pkl"
+    with open(classifier_path, 'rb') as f:
+        (model, class_names) = pickle.load(f)
+        print("Loaded classifier file")
+
+    with tf.Graph().as_default():
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        with sess.as_default():
+            # Bounding box
+            pnet, rnet, onet = align.detect_face.create_mtcnn(sess, "src\\align")
+
+            # Get the path of the facenet model and load it
+            facenet_model_path = os.getcwd() + "\\facenet_model\\20170512-110547\\20170512-110547.pb"
+            facenet.load_model(facenet_model_path)
+
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+            embedding_size = embeddings.get_shape()[1]
+
+            # Start video capture
+            people_detected = set()
+
+            person_detected = collections.Counter()
+
+            if args.webcam is True:
+                video_capture = cv2.VideoCapture(0)
+            else:
+                video_path = os.getcwd() + "\\test_data\\video\\"
+                video_name = "graham_norton"
+                full_original_video_path_name = video_path + video_name + '.mp4'
+                video_capture_path = full_original_video_path_name
+                if not os.path.isfile(full_original_video_path_name):
+                    print('Video not found at path ' + full_original_video_path_name + '. Commencing download from YouTube')
+                    # Note if the video ever gets removed this will cause issues
+                    YouTube(args.youtube_video_url).streams.first().download(output_path =video_path, filename=video_name)
+                video_capture = cv2.VideoCapture(full_original_video_path_name)
+            width = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+            height = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
+            video_recording = cv2.VideoWriter('output.avi', fourcc, 10, (int(width), int(height)))
+
+            total_frames_passed = 0
+
+            while True:
+                try:
+                    ret, frame = video_capture.read()
+                except Exception as e:
+                    break
+
+                # Skip frames if video is to be sped up
+                if args.video_speedup:
+                    total_frames_passed += 1
+                    if total_frames_passed % args.video_speedup != 0:
+                        continue
+
+                bounding_boxes, _ = align.detect_face.detect_face(frame, minsize, pnet, rnet, onet, threshold, factor)
+                faces_found = bounding_boxes.shape[0]
+
+                if faces_found > 0:
+                    det = bounding_boxes[:, 0:4]
+
+                    bb = np.zeros((faces_found, 4), dtype=np.int32)
+                    for i in range(faces_found):
+                        bb[i][0] = det[i][0]
+                        bb[i][1] = det[i][1]
+                        bb[i][2] = det[i][2]
+                        bb[i][3] = det[i][3]
+
+                        # inner exception
+                        if bb[i][0] <= 0 or bb[i][1] <= 0 or bb[i][2] >= len(frame[0]) or bb[i][3] >= len(frame):
+                            print('face is inner of range!')
+                            continue
+
+                        cropped = frame[bb[i][1]:bb[i][3], bb[i][0]:bb[i][2], :]
+                        scaled = cv2.resize(cropped, (input_image_size, input_image_size), interpolation=cv2.INTER_CUBIC)
+                        # cv2.imshow("Cropped and scaled", scaled)
+                        # cv2.waitKey(1)
+                        scaled = facenet.prewhiten(scaled)
+                        # cv2.imshow("\"Whitened\"", scaled)
+                        # cv2.waitKey(1)
+
+                        scaled_reshape = scaled.reshape(-1, input_image_size, input_image_size, 3)
+                        feed_dict = {images_placeholder: scaled_reshape, phase_train_placeholder: False}
+                        emb_array = sess.run(embeddings, feed_dict=feed_dict)
+                        predictions = model.predict_proba(emb_array)
+                        best_class_indices = np.argmax(predictions, axis=1)
+                        best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
+                        best_name = class_names[best_class_indices[0]]
+                        print("Name: {}, Probability: {}".format(best_name, best_class_probabilities))
+
+                        if best_class_probabilities > 0.09:
+                            cv2.rectangle(frame, (bb[i][0], bb[i][1]), (bb[i][2], bb[i][3]), (0, 255, 0), 2)    #boxing face
+                            text_x = bb[i][0]
+                            text_y = bb[i][3] + 20
+                            cv2.putText(frame, class_names[best_class_indices[0]], (text_x, text_y), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                        1, (0, 0, 255), thickness=1, lineType=2)
+                            person_detected[best_name] += 1
+
+                    # total_frames_passed += 1
+                    # if total_frames_passed == 2:
+                    for person, count in person_detected.items():
+                        if count > 4:
+                            print("Person Detected: {}, Count: {}".format(person, count))
+                            people_detected.add(person)
+                    # person_detected.clear()
+                    # total_frames_passed = 0
+
+
+                cv2.putText(frame, "People detected so far:", (20, 20), cv2.FONT_HERSHEY_PLAIN,
+                            1, (255, 0, 0), thickness=1, lineType=2)
+                currentYIndex = 40
+                for idx, name in enumerate(people_detected):
+                    cv2.putText(frame, name, (20, currentYIndex + 20 * idx), cv2.FONT_HERSHEY_PLAIN,
+                                1, (0, 0, 255), thickness=1, lineType=2)
+                cv2.imshow("Face Detection and Identification", frame)
+                video_recording.write(frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+    video_recording.release()
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    args = lambda : None
+    args.video = True
+    args.youtube_video_url = 'https://www.youtube.com/watch?v=WW1VmFi_18Y'
+    args.video_speedup = 5
+    args.webcam = False
+    main(args)
